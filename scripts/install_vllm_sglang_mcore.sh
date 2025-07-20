b317e261e606595902dcc6594eaaeacfb929ee7e
yesiam-png <shenaozhang@Shenaos-MacBook-Pro.local>
2025-07-19 17:58:57 -0700

    sglang install

--- scripts/install_vllm_sglang_mcore.sh
+++ scripts/install_vllm_sglang_mcore.sh
@@ -6,9 +6,7 @@ USE_SGLANG=${USE_SGLANG:-1}
 export MAX_JOBS=32
 
 echo "1. install inference frameworks and pytorch they need"
-if [ $USE_SGLANG -eq 1 ]; then
-    pip install "sglang[all]==0.4.6.post1" --no-cache-dir --find-links https://flashinfer.ai/whl/cu124/torch2.6/flashinfer-python && pip install torch-memory-saver --no-cache-dir
-fi
+pip install "sglang[all]==0.4.6.post3" --no-cache-dir --find-links https://flashinfer.ai/whl/cu124/torch2.6/flashinfer-python && pip install torch-memory-saver --no-cache-dir
 pip install --no-cache-dir "vllm==0.8.5.post1" "torch==2.6.0" "torchvision==0.21.0" "torchaudio==2.6.0" "tensordict==0.6.2" torchdata
 
 echo "2. install basic packages"
