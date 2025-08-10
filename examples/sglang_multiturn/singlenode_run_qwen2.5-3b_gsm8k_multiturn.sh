5a8f7c6fba290e23cdb9b3801fc6e00354576128
root <root@bolt-fh6fg7ukdn-p3559ddgtm.bolt-pods.turi-bolt.svc.cluster.local>
2025-08-10 23:46:06 +0000

    successful format and nextline

--- examples/sglang_multiturn/singlenode_run_qwen2.5-3b_gsm8k_multiturn.sh
+++ examples/sglang_multiturn/singlenode_run_qwen2.5-3b_gsm8k_multiturn.sh
@@ -25,7 +25,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=20 \
     +actor_rollout_ref.ref.prob_in_loss=True \
     +actor_rollout_ref.actor.prob_in_loss=True \
-    +actor_rollout_ref.actor.prob_in_loss_coeff=0.1 \
+    +actor_rollout_ref.actor.prob_in_loss_coeff=0.05 \
     +actor_rollout_ref.ref.prob_in_reward_coeff=1.0 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=80 \
     actor_rollout_ref.actor.use_kl_loss=False \
@@ -37,7 +37,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=32 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.name=sglang \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.9 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
     actor_rollout_ref.rollout.n=5 \
     +actor_rollout_ref.rollout.per_turn_response_length=16 \
     +actor_rollout_ref.rollout.max_code_lines=32 \
@@ -46,7 +46,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.critic_warmup=0 \
     trainer.logger='["console","wandb"]' \
     trainer.project_name='rl-code-cpt' \
-    trainer.experiment_name='noentropy-prob-mean-nostd-format1-probloss01-nostripinformat-ref' \
+    trainer.experiment_name='noentropy-prob-mean-nostd-format1-probloss005-withstripinformat-ref' \
     trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
     trainer.val_before_train=False \
